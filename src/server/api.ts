067a77a7fbc120bad669d47698b1556268c3a014
dmendez@creativ.ch <dmendez@creativ.ch>
2022-08-20 15:51:42 +0200

    todo app tutorial

--- src/server/api.ts
+++ src/server/api.ts
@@ -2,13 +2,19 @@ import { remultExpress } from 'remult/remult-express';
 import { AuthController } from '../shared/AuthController';
 import { Task } from '../shared/Task';
 import { TasksController } from '../shared/TasksController';
+import { createPostgresConnection } from "remult/postgres";
 
 export const api = remultExpress({
+    dataProvider: async () => {
+        if (process.env["NODE_ENV"] === "production")
+            return createPostgresConnection({ configuration: "heroku" });
+        return undefined;
+    },
     entities: [Task],
     controllers: [TasksController, AuthController],
     initApi: async remult => {
         const taskRepo = remult.repo(Task);
-        if (await taskRepo.count() !== 0)  return;
+        if (await taskRepo.count() !== 0) return;
         taskRepo.insert([
             { title: "Task a" },
             { title: "Task b", completed: true },
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import { api } from './api';
 import { expressjwt } from 'express-jwt';
+import helmet from 'helmet';
+import compression from 'compression';
+import sslRedirect from 'heroku-ssl-redirect';
+import path from 'path';
 
 const app = express();
+app.use(sslRedirect());
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(compression());
+
 app.use(expressjwt({
     secret: process.env['JWT_SECRET'] || "my secret",
     credentialsRequired: false,
@@ -10,4 +18,9 @@ app.use(expressjwt({
 }));
 app.use(api);
 
-app.listen(3002, () => console.log("Server started"));
+app.use(express.static(path.join(__dirname, '../remult-angular-todo')));
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, '../remult-angular-todo', 'index.html'));
+});
+
+app.listen(process.env["PORT"] || 3002, () => console.log("Server started"));
